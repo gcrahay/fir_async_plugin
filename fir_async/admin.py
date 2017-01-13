4ea7b6fca1b649e1a8fb95a51d23c14affd11304
Gaetan Crahay <gaetan@crahay.eu>
2017-01-13 10:28:53 +0100

    Improve notification templates admin list

--- fir_async/admin.py
+++ fir_async/admin.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib import admin
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _, pgettext_lazy
+
 from fir_async.registry import registry
 from fir_plugins.admin import MarkdownModelAdmin
 from fir_async.models import MethodConfiguration, NotificationTemplate, NotificationPreference
@@ -16,6 +18,14 @@ class NotificationTemplateForm(forms.ModelForm):
 class NotificationTemplateAdmin(MarkdownModelAdmin):
     markdown_fields = ('description', 'short_description')
     form = NotificationTemplateForm
+    list_display = ('event', 'business_lines_list')
+
+    def business_lines_list(self, obj):
+        bls = obj.business_lines.all()
+        if bls.count():
+            return ', '.join([bl.name for bl in bls])
+        return pgettext_lazy('business lines', 'All')
+    business_lines_list.short_description = _('Business lines')
 
 
 admin.site.register(NotificationTemplate, NotificationTemplateAdmin)
